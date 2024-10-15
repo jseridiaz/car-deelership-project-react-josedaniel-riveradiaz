849aa450184b93ab17ae963d50a4540d241e6f3d
jseridiaz <jseridiaz@gmail.com>
2024-10-15 22:09:40 +0200

    fix cors

--- BACK/index.js
+++ BACK/index.js
@@ -4,24 +4,21 @@ const express = require("express")
 const { bbddConection } = require("./src/config/db")
 const cors = require("cors")
 const fs = require("fs")
-const routerAuto = require("./src/API/routes/auto")
 const AutoModel = require("./src/API/models/auto")
-const { rgbToHex } = require("./src/utils/functions/rgbTohex")
-const { getColor } = require("./src/utils/functions/getColor")
-const routerUser = require("./src/API/routes/user")
+
 const routerMain = require("./src/API/routes/main/mainRoute")
 const { convertCsvToJs } = require("./src/utils/functions/convertCsvToJs")
 const { cloudinaryConnect } = require("./src/config/cloudinary")
+const app = express()
+app.use(cors())
 bbddConection()
 cloudinaryConnect()
-const app = express()
+
 app.use(express.json())
 
 const jsonAuto = []
 
 convertCsvToJs(jsonAuto)
-
-app.use(cors())
 
 app.use("/", routerMain)
 
