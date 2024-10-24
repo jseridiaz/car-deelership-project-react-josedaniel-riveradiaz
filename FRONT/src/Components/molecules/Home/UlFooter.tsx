8ff2de8ecffed3554d21051984f46bed65aa602e
jseridiaz <jseridiaz@gmail.com>
2024-10-24 11:35:27 +0200

    UPLOAD footer

--- FRONT/src/Components/molecules/Home/UlFooter.tsx
+++ FRONT/src/Components/molecules/Home/UlFooter.tsx
@@ -8,17 +8,17 @@ const UlFooter: React.FC<UlFooterType> = ({ title, arrayLi }) => {
    return (
       <ul className='min-w-[120px] w-full sm:w-1/4 flex flex-wrap flex-col text-left p-1 items-center'>
          <li className='font-medium text-left'>{title}</li>
-         <div className='flex flex-col mt-2 gap-2 '>
+         <li className='flex flex-col mt-2 gap-2 '>
             {arrayLi.map((el, idx) => (
                <Link
                   key={idx}
                   className='font-normal text-gray-200 hover:underline underline-offset-2 p-1 text-sm '
                   to={el.to}
                >
-                  <li className='w-full'>{el.name}</li>
+                  {el.name}
                </Link>
             ))}
-         </div>
+         </li>
       </ul>
    )
 }
