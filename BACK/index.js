3d7c2fde402ea2c1387ceaa4b85bad6bc614b8d3
jseridiaz <jseridiaz@gmail.com>
2024-10-24 10:10:15 +0200

    feat read fs

--- BACK/index.js
+++ BACK/index.js
@@ -3,7 +3,7 @@ const express = require("express")
 
 const { bbddConection } = require("./src/config/db")
 const cors = require("cors")
-// const fs = require("fs")
+const fs = require("fs")
 const AutoModel = require("./src/API/models/auto")
 
 const routerMain = require("./src/API/routes/main/mainRoute")
@@ -19,24 +19,24 @@ app.use(express.json())
 const jsonAuto = []
 
 convertCsvToJs(jsonAuto)
-// fs.writeFile("Autos.json", JSON.stringify(jsonAuto), async error => {
-//    if (error) {
-//       console.log(error)
-//    } else {
-//       const allAutos = await AutoModel.find()
-//       const allAutosVin = allAutos.map(el => el.vin)
+fs.writeFile("Autos.json", JSON.stringify(jsonAuto), async error => {
+   if (error) {
+      console.log(error)
+   } else {
+      const allAutos = await AutoModel.find()
+      const allAutosVin = allAutos.map(el => el.vin)
 
-//       for (let i = 0; i < jsonAuto.length; i++) {
-//          const e = jsonAuto[i].vin
-//          if (allAutosVin.includes(e)) {
-//             continue
-//          } else {
-//             await AutoModel.insertMany(jsonAuto[i])
-//             console.log("writted")
-//          }
-//       }
-//    }
-// })
+      for (let i = 0; i < jsonAuto.length; i++) {
+         const e = jsonAuto[i].vin
+         if (allAutosVin.includes(e)) {
+            continue
+         } else {
+            await AutoModel.insertMany(jsonAuto[i])
+            console.log("writted")
+         }
+      }
+   }
+})
 
 app.use("/", routerMain)
 app.use("*", (req, res, next) => {
