376e14960bb4f0902822ff871c02d210dcded1d5
jseridiaz <jseridiaz@gmail.com>
2025-02-05 19:55:03 +0100

    upload fetch

--- FRONT/src/Components/molecules/Home/Explore.tsx
+++ FRONT/src/Components/molecules/Home/Explore.tsx
@@ -51,6 +51,8 @@ const Explore = () => {
    )
 
    useEffect(() => {
+      console.log(arrayAutos)
+
       fetchData(filterSelect, setArrayAutos)
    }, [])
    useEffect(() => {
--- FRONT/src/utils/functions/fetch/fetchGetAutos.ts
+++ FRONT/src/utils/functions/fetch/fetchGetAutos.ts
@@ -12,19 +12,17 @@ const fetchGetAutos = async (
    minYear?: number,
    maxYear?: number,
 ): Promise<AutoModelType[]> => {
-   console.log(import.meta.env.VITE_URL_BASE)
-
    const Respons = await fetch(
       `${import.meta.env.VITE_URL_BASE + "/search/query?"}${
-         availability ? "availability=Disponible" : ""
-      }${brand && brand != "All" ? `&brand=${brand}` : ""}${
-         model && model != "All" ? `&model=${model}` : ""
-      }${chassis && chassis != "All" ? `&chassis=${chassis}` : ""}${
-         minPrice ? `&minPrice=${minPrice}` : ""
-      }${maxPrice ? `&maxPrice=${maxPrice}` : ""}${minKm ? `&minKm=${minKm}` : ""}${
-         maxKm ? `&maxKm=${maxKm}` : ""
-      }${minYear ? `&minYear=${minYear}` : ""}${
-         maxYear ? `&maxYear=${maxYear}` : ""
+         availability ? "availability=Disponible" : null
+      }${brand && brand != "All" ? `&brand=${brand}` : null}${
+         model && model != "All" ? `&model=${model}` : null
+      }${chassis && chassis != "All" ? `&chassis=${chassis}` : null}${
+         minPrice ? `&minPrice=${minPrice}` : null
+      }${maxPrice ? `&maxPrice=${maxPrice}` : null}${
+         minKm ? `&minKm=${minKm}` : null
+      }${maxKm ? `&maxKm=${maxKm}` : null}${minYear ? `&minYear=${minYear}` : null}${
+         maxYear ? `&maxYear=${maxYear}` : null
       }`,
    )
       .then(res => res.json())
