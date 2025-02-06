8a3834970eeaf2b8bba33e92119231fd810a9db9
jseridiaz <jseridiaz@gmail.com>
2025-02-06 17:22:04 +0100

    color nav

--- FRONT/src/Components/molecules/Seo.tsx
+++ FRONT/src/Components/molecules/Seo.tsx
@@ -7,7 +7,7 @@ const Seo: React.FC<SeoType> = ({ title, description, url, img }) => {
       <Helmet>
          <title>{title}</title>
          <meta name='description' content={description} />
-         <meta name='theme-color' content='transparent' />
+         <meta name='theme-color' content='#fffff' />
          <meta property='og:title' content={title} />
          <meta property='og:type' content='website' />
          <meta property='og:url' content={url} />
