33a4834bd6d12a130fa2b42ba07841ee24ac7146
jseridiaz <jseridiaz@gmail.com>
2025-02-06 17:12:21 +0100

    set Theme-color

--- FRONT/src/Components/molecules/Seo.tsx
+++ FRONT/src/Components/molecules/Seo.tsx
@@ -7,6 +7,7 @@ const Seo: React.FC<SeoType> = ({ title, description, url, img }) => {
       <Helmet>
          <title>{title}</title>
          <meta name='description' content={description} />
+         <meta name='theme-color' content='transparent' />
          <meta property='og:title' content={title} />
          <meta property='og:type' content='website' />
          <meta property='og:url' content={url} />
