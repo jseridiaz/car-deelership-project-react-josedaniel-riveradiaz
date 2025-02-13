0dd9f941846298c50cc087dd144202f152443b82
jseridiaz <jseridiaz@gmail.com>
2025-02-13 12:15:52 +0100

    upload .test

--- FRONT/src/utils/test/H1component.test.ts
+++
@@ -1,8 +0,0 @@
-import { expect, test } from "vitest"
-import { render } from 'vitest-browser-react'
-
-import H1Component from "../../Components/atoms/H1Component"
-
-function renderComponent=()=>{
-    
-}
